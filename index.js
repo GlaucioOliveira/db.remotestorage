0f1d6106bfdd62904f5676d50184ab34e85366dd
Gláucio Oliveira <glaucio.dev@gmail.com>
2024-08-09 13:05:56 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,153 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3001;
+
+const db_host = process.env.HOST;
+const db_user = process.env.USER;
+const db_pass = process.env.PASS;
+const db_database = process.env.DATABASE;
+
+const apiBase = process.env.API_BASE;
+
+const db = mysql.createConnection({
+  host: db_host,
+  user: db_user,
+  password: db_pass,
+  database: db_database
+});
+
+function connectDatabase()
+{
+  db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ', err);
+    setTimeout(connectDatabase, 10000);
+  } else {
+    console.log('Connected to MySQL');
+  }
+});
+}
+
+connectDatabase();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Get Remote Storage
+app.get(`${apiBase}/:key/:secret`, (req, res) => {
+  const key = req.params.key;
+  const secret = req.params.secret;
+  const sql = 'SELECT `key`, `value` from remotestorage where `key` = ? and `secret` = ?';
+
+  try{
+    db.query(sql, [key, secret], (err, result) => {
+      if (err) {
+        console.error('Error getting meeting: ', err);
+        res.status(500).send('Error getting meeting');
+      } else {
+        res.status(200).json(result[0]);
+      }
+    });
+  }
+  catch(ex){
+    res.status(500).send(ex);
+  }
+});
+
+// Create
+app.post(`${apiBase}`, (req, res) => {
+    const {key, value, secret} = req.body;
+
+  const sql = 'INSERT INTO remotestorage values (0, ?, ?, ?)';
+
+  try{
+      db.query(sql, [key, value, secret], (err, result) => {
+    if (err) {
+      console.error('Error creating remote storage: ', err);
+      res.status(500).send('Error creating remote storage');
+    } else {
+      res.status(201).json({Id: result.insertId});
+    }
+  });
+  }
+  catch(ex){
+    res.status(500).send(ex);
+  }
+
+});
+
+app.put(`${apiBase}`, (req, res) => {
+    //const key = req.params.key;
+    const {key, value, secret} = req.body;
+    let Id = 0;
+
+    try{    
+    db.query('select Id from remotestorage where `key` = ? and `secret` = ?', [key, secret], (err, result) => {
+      if (err) {
+        console.error('something worng happened while trying to verify if remotesotrage exists: ', err);
+      } else {
+        if(result && result[0] && result[0].Id > 0)
+        {
+          Id = Id = result[0].Id;
+          
+          const sql = 'UPDATE remotestorage SET value = ? where Id = ?';
+          console.log(sql);
+    
+          db.query(sql, [value, Id], (err, result) => {
+            if (err) {
+              console.error('Error getting remote storage: ', err);
+              res.status(500).send('Error getting remote storage');
+            } else {
+              res.status(200).json(result[0]);
+            }
+          });
+        }
+        else{
+          const sql = 'INSERT INTO remotestorage values (0, ?, ?, ?)';
+          console.log(sql);
+    
+          db.query(sql, [key, value, secret], (err, result) => {
+            if (err) {
+              console.error('Error creating remote storage: ', err);
+              res.status(500).send('Error creating remote storage');
+            } else {
+              res.status(201).json({Id: result.insertId});
+            }
+          });
+        }        
+      }
+    });
+  }
+  catch(ex){
+    res.status(500).send(ex);
+  }
+
+});
+
+// Delete
+app.delete(`${apiBase}/:key/:secret`, (req, res) => {
+  const key = req.params.key;
+  const secret = req.params.secret;
+  const sql = 'DELETE FROM reuniao WHERE `key` = ? and `secret` = ?';
+
+  try{
+      db.query(sql, [key, secret], (err, result) => {
+    if (err) {
+      console.error('Error deleting remote storage: ', err);
+      res.status(500).send('Error deleting remote storage');
+    } else {
+      res.status(200).send('remote storage deleted successfully');
+    }
+  });
+  }
+  catch(ex)
+  {
+    res.status(500).send(ex);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
